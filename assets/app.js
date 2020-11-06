789b800eb984717b997628cc2e089e721c002545
Isaiah <isaiah.robinson1029@gmail.com>
2020-11-06 10:05:26 -0500

    Basic Styling
    The basic style of the site has been slightly improved
    - added CSS design
    - card style layout and title bar

--- assets/app.js
+++ assets/app.js
@@ -1,14 +1,16 @@
-// VARIABLES
+// V A R I A B L E S
 // The element data will be sent to
 let dogContainer = document.getElementById("dogData");
 // Button to load more user results
 let loadMore = document.getElementById("loadMore");
 // Page limit tracking
 let userCount = 25;
+let urls = ['https://jsonplaceholder.typicode.com/comments', 'https://random.dog/woof.json'];
+let imgArray = [];
 
 // JSON DATA FETCH
 let grabData = (userCount) => {
-  fetch('https://jsonplaceholder.typicode.com/comments')
+  fetch(urls[0])
     .then(response => {
       return response.json();
     })
@@ -26,12 +28,13 @@ let fillPage = (dogs, count) => {
   // User element container
   let content = "";
   for (let i = 0; i < count; i++) {
+
     // Variable to hold eat user element
     content += `
-    <div class="users" id=${dogs[i].id}>
-    <h3 class="name" id="${dogs[i].postId}">${dogs[i].name}<h3>
-    <p class="email">${dogs[i].email}<p>
-    <p class="description">${dogs[i].body}<p>
+    <div class="user" id=${dogs[i].id}>
+    <h3 class="name heading" id="${dogs[i].postId}">${dogs[i].name}</h3>
+    <p class="email info">${dogs[i].email}</p>
+    <p class="description">${dogs[i].body}</p>
     </div>`;
   }
   // Increase user limit tracking
