0ee2161977180ddd69eae200659880a3d49e2002
Isaiah <isaiah.robinson1029@gmail.com>
2020-11-07 13:53:36 -0500

    Finished JSON Display Functionality
    completed working on JSON fetching and display
    - added another source of JSON data being dog images
    - incorporated user images into the style sheets

--- assets/app.js
+++ assets/app.js
@@ -6,7 +6,6 @@ let loadMore = document.getElementById("loadMore");
 // Page limit tracking
 let userCount = 25;
 let urls = ['https://jsonplaceholder.typicode.com/comments', 'https://random.dog/woof.json'];
-let imgArray = [];
 
 // JSON DATA FETCH
 let grabData = (userCount) => {
@@ -25,28 +24,41 @@ let grabData = (userCount) => {
 
 // Populate page function
 let fillPage = (dogs, count) => {
-  // User element container
-  let content = "";
-  for (let i = 0; i < count; i++) {
-
-    // Variable to hold eat user element
-    content += `
-    <div class="user" id=${dogs[i].id}>
-    <h3 class="name heading" id="${dogs[i].postId}">${dogs[i].name}</h3>
-    <p class="email info">${dogs[i].email}</p>
-    <p class="description">${dogs[i].body}</p>
-    </div>`;
+  // Loop for the NEXT 25 users added to page, from 0
+  for (let i = count - 25; i < count; i++) {
+    // User element container
+    let content = document.createElement('div');
+    // Grab Photos with another fetch
+    fetch(urls[1])
+      .then(data => data.json())
+      .then(imgs => {
+        content.className = `user`; // assign Class
+        content.id = `${dogs[i].id}`; // assign ID
+        // Exclude video formats
+        if (!imgs.url.includes("mp4" || "webm")) {
+          content.innerHTML += `
+          <img src="${imgs.url}" class="profilePhoto">`;
+        } else {
+          content.innerHTML += `
+          <img src="./assets/img/dogImg.jpg" class="profilePhoto">`;
+        }
+        content.innerHTML += `
+        <h3 class="name heading" id="${dogs[i].postId}">${dogs[i].name}</h3>
+        <p class="email info">${dogs[i].email}</p>
+        <p class="description">${dogs[i].body}</p>`;
+        content.innerHTML += `</div>`;
+      });
+    // append content to document
+    dogContainer.append(content);
   }
   // Increase user limit tracking
   userCount += 25;
-
-  // Fill page with data
-  dogContainer.innerHTML = content;
 }
 
 // EVENT HANDLERS
 loadMore.addEventListener("click", loadUsers => {
-  if (userCount >= 525) {
+  // Maximum users reached
+  if (userCount >= 501) {
     console.log(`User Limit Reached: 500`);
     return;
   } else {
